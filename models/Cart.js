f33df00a48a2d776c502ee6f42c933db6f18be77
Adebola <onedebos@gmail.com>
2020-07-09 09:50:52 +0100

    Setup models

---
+++ models/Cart.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const config = require('../utils/config');
+
+mongoose.connect(config.DB_LINK, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const cartSchema = mongoose.Schema({
+  courseTitile: {
+    type: String,
+    required: true,
+  },
+  author: String,
+  url: {
+    type: String,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  rating: {
+    type: Number,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+cartSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3,
+  },
+  name: String,
+  password: {
+    type: String,
+  },
+  passwordHash: String,
+  carts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Cart',
+    },
+  ],
+});
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
