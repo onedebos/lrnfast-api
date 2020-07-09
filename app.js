c279e57ed723d6c8b9f0ac3a385d2223e97e8ef2
Adebola <onedebos@gmail.com>
2020-07-09 11:34:59 +0100

    update usersRoute

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const app = require('./index');
 const cartRoute = require('./controller/cartRouter');
-const userRoute = require('./controller/userRouter');
+const usersRoute = require('./controller/userRouter');
 const loginRoute = require('./controller/loginRouter');
 const middleware = require('./utils/middleware');
 
 app.use(middleware.tokenExtractor);
 app.use('/api/carts', cartRoute);
-app.use('/api/user', userRoute);
+app.use('/api/users', usersRoute);
 app.use('/api/login', loginRoute);
 
 app.use(middleware.unknownEndPoint);
