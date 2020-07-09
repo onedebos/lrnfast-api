7d98d31ebc0e1dd8a28455c19bf1c4f5be4e20b9
Adebola <onedebos@gmail.com>
2020-07-09 09:24:48 +0100

    setup server

---
+++ utils/middleware.js
@@ -0,0 +1,15 @@
+const unknownEndPoint = (request, response) => {
+  response.status(404).send({ error: "Endpoint does not exist" });
+};
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+
+  if (authorization && authorization.toLowerCase().startsWith("bearer")) {
+    request.token = authorization.substring(7);
+  }
+
+  next();
+};
+
+module.exports = { unknownEndPoint, tokenExtractor };
